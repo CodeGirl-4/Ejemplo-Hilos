b0e2df85a1e4d926b7b7947fc2896181f2a6b0b8
Carolyn Báez <carobaezsoriano@gmail.com>
2021-07-10 00:08:01 -0400

    First Commit

---
+++ Ejemplo_Hilos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Ejemplo_Hilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--Ejemplo de programación por multihilos--");
+            Console.WriteLine();
+            Console.WriteLine("**************************************************************************************************");
+
+            //Creación de hilos
+            Thread Chef1 = new Thread(new ThreadStart(chef_1));
+            Thread Chef2 = new Thread(new ThreadStart(chef_2));
+            Thread Chef3 = new Thread(new ThreadStart(chef_3));
+
+            //Invocando métodos
+            Chef1.Start();
+            Chef2.Start();
+            Chef3.Start();
+        }
+
+        public static void chef_1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Chef cocinando pizza...");
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+        }
+
+        public static void chef_2()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Chef cocinando sopa...");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+
+        public static void chef_3()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Chef cocinando pollo...");
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
